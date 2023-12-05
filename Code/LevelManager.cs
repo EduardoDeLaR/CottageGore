e644658a45d9b20c9dc9e99dca1889c09832a293
Eduardo De La Rosa <72276457+EduardoDeLaR@users.noreply.github.com>
2023-12-04 21:51:04 -0700

    Update LevelManager.cs

--- Code/LevelManager.cs
+++ Code/LevelManager.cs
@@ -3,21 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Define the LevelManager class inheriting from MonoBehaviour
 public class LevelManager : MonoBehaviour
 {
+    // Static instance for Singleton pattern
     private static LevelManager _instance;
 
+    // SerializeField allows this to be set in the Unity Editor
     [SerializeField] private int lives = 10;
 
+    // Public properties to access and set total lives and the current wave
     public int TotalLives { get; set; }
     public int CurrentWave { get; set; }
 
+    // Public action delegate for handling end-reached events
     public Action<Shroom> OnEndReached;
 
+    // Property for accessing the Singleton instance of LevelManager
     public static LevelManager Instance
     {
         get
         {
+            // If an instance doesn't exist, find it or create a new one
             if (_instance == null)
             {
                 _instance = FindObjectOfType<LevelManager>();
@@ -31,27 +38,34 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    // Awake is called when the script instance is being loaded
     private void Awake()
     {
+        // Initialize the Singleton instance
         if (_instance == null)
         {
             _instance = this;
         }
-        else
+        else if (_instance != this)
         {
+            // Ensure that there is only one instance in the scene
             Destroy(gameObject);
         }
     }
 
+    // Start is called before the first frame update
     private void Start()
     {
+        // Initialize total lives and the current wave
         TotalLives = lives;
         CurrentWave = 1;
     }
 
+    // Method to reduce lives when an enemy reaches the end
     private void ReduceLives(Shroom enemy)
     {
         TotalLives--;
+        // Check if the lives have run out
         if (TotalLives <= 0)
         {
             TotalLives = 0;
@@ -59,25 +73,29 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    // Method to handle game over scenario
     private void GameOver()
     {
-        // Game over logic
-
+        // Implement game over logic
     }
 
+    // Method to handle completion of a wave
     private void WaveCompleted()
     {
-        // Wave completed logic
-
+        // Implement wave completed logic
     }
 
+    // OnEnable is called when the object becomes enabled and active
     private void OnEnable()
     {
+        // Subscribe to the OnEndReached event
         OnEndReached += ReduceLives;
     }
 
+    // OnDisable is called when the object becomes disabled or inactive
     private void OnDisable()
     {
+        // Unsubscribe from the OnEndReached event
         OnEndReached -= ReduceLives;
     }
 }
